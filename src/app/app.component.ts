47757cfcb8deea775cf09c33f6f56f5173e4554d
barrobaye <105419057+barrobaye@users.noreply.github.com>
2025-07-16 13:52:30 +0000

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {HeaderComponent} from './other/header/header.component';
-import {FooterComponent} from './other/footer/footer.component';
+
 
 @Component({
   selector: 'app-root',
+<<<<<<< Updated upstream
   imports: [
     RouterOutlet,
     HeaderComponent,
     FooterComponent
-  ],
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
